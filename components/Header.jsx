e3dedb1bf10c64d5d3aba3e1f5a1069c712ba116
MAX <madmax007@yandex.ru>
2020-01-11 23:24:17 +0300

    git commit

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header = () =>{
+    return(
+        <header className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR2k_tiwdG0pwTWaT-f-NrCyHHmDVhgCKAvTxNlEGv4YZFu_tXC" />
+      </header>
+    );
+}
+export default Header;
---
+++ components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+const Profile = () =>{
+    return(
+      <div className="content">
+        <div>
+          <img src="https://avatars.mds.yandex.net/get-pdb/2110190/cd9d3249-f960-4df2-8375-9e1a86f8d2f3/orig" />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post1
+            </div>
+            <div>
+              post2
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+export default Profile;
